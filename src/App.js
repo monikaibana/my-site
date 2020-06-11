9a7ceea53c6129fa7ad66b8dd2ced0d95a91c9bd
Monika Ibana <monika@Monikas-MacBook-Pro.local>
2020-06-10 23:39:31 -0400

    github deployment troubleshooting

--- src/App.js
+++ src/App.js
@@ -7,7 +7,6 @@ import Projects from "./components/Projects.js";
 import Contact from "./components/Contact.js";
 import logo from "./components/images/logo-new.png";
 import $ from "jquery";
-import { BrowserRouter as Router } from "react-router-dom";
 
 $(function () {
   var header = $(".navbar");
@@ -24,53 +23,51 @@ $(function () {
 
 function App() {
   return (
-    <Router>
-      <div className="app">
-        <div id="home">
-          <Home />
-        </div>
-        <div id="about">
-          <About />
-        </div>
-        <div id="work">
-          <Work />
-        </div>
-        <div id="resume">
-          <Resume />
-        </div>
-        <div id="projects">
-          <Projects />
-        </div>
-        <div id="contact">
-          <Contact />
-        </div>
-        <div className="trademark">Some kinda trademark idk</div>
-        <div className="navbar">
-          <div className="logo">
-            <a href="#home">
-              <img href="#home" src={logo} alt="logo" />
-            </a>
-          </div>
-          <ul className="navbar-items">
-            <a href="#about">
-              <li>About</li>
-            </a>
-            <a href="#work">
-              <li>Work</li>
-            </a>
-            <a href="#resume">
-              <li>Resume</li>
-            </a>
-            <a href="#projects">
-              <li>Projects</li>
-            </a>
-            <a href="#contact">
-              <li>Contact</li>
-            </a>
-          </ul>
-        </div>
+    <div className="app">
+      <div id="home">
+        <Home />
       </div>
-    </Router>
+      <div id="about">
+        <About />
+      </div>
+      <div id="work">
+        <Work />
+      </div>
+      <div id="resume">
+        <Resume />
+      </div>
+      <div id="projects">
+        <Projects />
+      </div>
+      <div id="contact">
+        <Contact />
+      </div>
+      <div className="trademark">Some kinda trademark idk</div>
+      <div className="navbar">
+        <div className="logo">
+          <a href="#home">
+            <img href="#home" src={logo} alt="logo" />
+          </a>
+        </div>
+        <ul className="navbar-items">
+          <a href="#about">
+            <li>About</li>
+          </a>
+          <a href="#work">
+            <li>Work</li>
+          </a>
+          <a href="#resume">
+            <li>Resume</li>
+          </a>
+          <a href="#projects">
+            <li>Projects</li>
+          </a>
+          <a href="#contact">
+            <li>Contact</li>
+          </a>
+        </ul>
+      </div>
+    </div>
   );
 }
 
